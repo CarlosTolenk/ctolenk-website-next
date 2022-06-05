fe241b159855e8efc9a10ea9ff64b5ee7aaf75a7
CarlosTolenk <carlos.tolentinoe@gmail.com>
2022-06-05 19:21:52 -0400

    Implement 404: - Fix/Images http

--- pages/404.tsx
+++ pages/404.tsx
@@ -10,29 +10,27 @@ interface IPropsNotFound404 {
 export default function NotFound404({title, description}: IPropsNotFound404) {
 
     return (
-        <>
-            <MainLayout title={title} description={description}>
-                <div className="error animate__animated animate__bounce">
-                    <div className="container">
-                        <div className="row justify-content-center">
-                            <div className="col-xs-12 col-sm-12 col-md-12 col-lg-6">
-                                <div className="error-content">
-                                    <div className="error-message">
-                                        <h2>404</h2>
-                                        <h3><span>Ooooops!</span> Something Went Wrong...</h3>
-                                    </div>
-                                    <div className="description">
+        <MainLayout title={title} description={description}>
+            <div className="error animate__animated animate__bounce">
+                <div className="container">
+                    <div className="row justify-content-center">
+                        <div className="col-xs-12 col-sm-12 col-md-12 col-lg-6">
+                            <div className="error-content">
+                                <div className="error-message">
+                                    <h2>404</h2>
+                                    <h3><span>Ooooopss!</span> Something Went Wrong...</h3>
+                                </div>
+                                <div className="description">
                                         <span>Or Goto
                                              <Link href={'/'} passHref><a> Homepage</a></Link>
                                         </span>
-                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </MainLayout>
-        </>
+            </div>
+        </MainLayout>
     )
 }
 
