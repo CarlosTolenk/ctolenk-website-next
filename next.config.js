dfb0dfdf3058c047f8f916494d778e240a4eb3e7
CarlosTolenk <carlos.tolentinoe@gmail.com>
2022-06-06 18:24:21 -0400

    FIX: CI/CD

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: '/cv-online.ctolenk.com',
+    basePath: '/',
     trailingSlash: true,
     reactStrictMode: true,
     images: {
